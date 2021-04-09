28242162c9a016d5c545d23fd27e4cef37639769
Emanoel Eike Ferreira da Silva <eike.em.2017@gmail.com>
2021-04-09 19:03:38 -0300

    função Arrow

---
+++ função/ArrowFunções.js
@@ -0,0 +1,18 @@
+let dobro = function(a){
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a //return implicito
+console.log(Math.PI)
+
+let ola = function () {
+    return 'ola'
+}
+
+ola = () => 'Olá'
+ola = _ => 'Olá' //possui um parametro
+console.log(ola())
