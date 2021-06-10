793f65cf7b1280e1ea0358f2943a64626da58863
Emanoel Eike Ferreira da Silva <eike.em.2017@gmail.com>
2021-06-10 01:32:47 -0300

    Orintação a objeto

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+//OO
+
+object = {
+    valor1, //valores
+    valor2,
+    valor3,
+    processamento(){ // comportamento
+        //...
+    }
+}
+
+object.processamento() // Foco passou a ser o 
+
+// Principios importantes
+// 1. abstração
+// 2. encpsulamento
+// 3. herança (prototype)
+// 4. polimorlifismo
